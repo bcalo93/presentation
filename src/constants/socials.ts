1d0fbdab54ea784f4adfd76a8d3e6693b8d3bb36
Brahian  Calo <bcalo93@gmail.com>
2026-01-04 10:21:29 -0300

    feature: introduction card (#2)
    * Refactor layout and styles; add SummaryCard component
    
    - Update reset styles to include margin for html, body, and h1 elements.
    - Change Header position from fixed to sticky and add margin and z-index.
    - Simplify Home component by removing unnecessary main wrapper and styles.
    - Introduce new SummaryCard component for displaying summary information.
    - Modify Layout component to include SummaryCard and adjust layout structure with left and right content areas.
    
    * Add profile photo, social links, and image dimension utility
    
    - Introduce a new profile photo asset for the SummaryCard component.
    - Create SocialLinks component to display social media links using SVG icons.
    - Add utility function to calculate image dimensions based on aspect ratio.
    - Update SummaryCard to include profile photo and social links, enhancing the layout and presentation.
    
    * Update SummaryCard content and refine reset styles
    
    - Modify the SummaryCard component to update the description text to "Software Technical Leader from Uruguay."
    - Refine reset styles by removing margin for the h1 element, ensuring a cleaner layout.

---
+++ src/constants/socials.ts
@@ -0,0 +1,21 @@
+import GithubLogo from '@/assets/svgs/github-logo.svg';
+import XLogo from '@/assets/svgs/x-logo.svg';
+import LinkedinLogo from '@/assets/svgs/linkedin-logo.svg';
+
+export const SOCIAL_LINKS = [
+  {
+    id: 'github',
+    href: 'https://github.com/bcalo93',
+    asset: GithubLogo,
+  },
+  {
+    id: 'x',
+    href: 'https://x.com/brahiancalo',
+    asset: XLogo,
+  },
+  {
+    id: 'linkedin',
+    href: 'https://www.linkedin.com/in/brahian-calo-suhr/',
+    asset: LinkedinLogo,
+  }
+]
